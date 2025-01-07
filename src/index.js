806f4a70f93c5288352d99fd34c609781eb80083
Marco LA <marco.la@student-cs.fr>
2025-01-07 13:34:42 +0100

    Correction du chemin racine pour GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
   <BrowserRouter basename="/">
