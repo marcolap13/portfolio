36caedbaa4f54660dd8195a43daeec8150a8a1a0
Marco LA <marco.la@student-cs.fr>
2025-01-07 13:06:55 +0100

    TESTE

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+
+
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter basename="/portfolio">
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </BrowserRouter>,
+  document.getElementById('root')
 );
