282246e6670110b5e1b4096330b2400bb91b476c
Marco LA <marco.la@student-cs.fr>
2025-01-07 13:48:33 +0100

    test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/portfolio">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
